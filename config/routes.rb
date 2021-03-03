8e51494bd3f63d2387eaece70d7bdeddce8e9939
augustin desalos <augustin.desalos@gmail.com>
2021-03-03 08:59:56 +0100

    last commit day 3 rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [ :index, :show, :new, :create ] do
+  resources :restaurants, only: [ :index, :show, :new, :create, :destroy ] do
     resources :reviews, only: [ :new, :create, :destroy ]
   end
 end
