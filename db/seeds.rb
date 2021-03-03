5ad5a9ea221027b5f3fc819339040da1947cfe74
augustin desalos <augustin.desalos@gmail.com>
2021-03-03 08:43:38 +0100

    exo 2 restaurant + review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0765456534", category: "french" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0989786732", category: "chinese" }
+pizza_hut =  { name: "Pizza Hut", address: "33 rue de mon nez", phone_number: "0989786732", category: "italian" }
+wagon =  { name: "Le Wagon", address: "Marseille bébé", phone_number: "09897863212", category: "french" }
+
+[ dishoom, pizza_east, pizza_hut, wagon ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
